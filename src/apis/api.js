64184373dfed461df70db7e8c2c3c3e6632dad7c
Amaraegbu Jedidiah <imunacode@gmail.com>
2019-11-18 23:19:06 +0100

    second deployment to heroku

--- src/apis/api.js
+++ src/apis/api.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: window.location.href.includes("localhost")
     ? "http://localhost:8000/api"
-    : "",
+    : "https://temp-benshada-alpha-api.herokuapp.com/api",
   headers: { "Content-Type": "application/json" }
 });
