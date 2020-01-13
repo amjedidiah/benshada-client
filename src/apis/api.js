5e3794aa7c5a463133de16f0d584bd272d511dd4
amjedidiah <imunacode@gmail.com>
2020-01-14 00:50:56 +0100

    Milestone 1 Report 3 Heroku

--- src/apis/api.js
+++ src/apis/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: window.location.href.includes("localhost")
-    ? "https://localhost:8000/api"
+    ? "http://localhost:8000/api"
     : "https://temp-benshada-alpha-api.herokuapp.com/api",
   headers: { "Content-Type": "application/json" }
 });
