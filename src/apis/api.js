a9c33376a02f7f0465a9d283727c389b64e2f752
amjedidiah <imunacode@gmail.com>
2020-05-04 15:35:57 +0100

    fix: https error && api baseURL

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: window.location.href.includes("localhost")
-    ? "http://localhost:8000/api"
-    : "https://temp-benshada-alpha-api.herokuapp.com/api",
+  baseURL: "https://benshada-api.herokuapp.com/api",
   headers: { "Content-Type": "application/json" }
 });
