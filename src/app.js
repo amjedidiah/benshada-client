fb3b2585dcaca825467ca574e180b4b159fe4d10
amjedidiah <imunacode@gmail.com>
2020-06-01 16:55:32 +0100

    fix: Fixed Codacy && Heroku errors

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import LoadingScreen from 'react-loading-screen';
 
-import Home from './components/home/home.js';
+import Home from './components/home/Home.js';
 import FormToast from './components/formToast/formToast.js';
 
 class App extends Component {
